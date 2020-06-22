ac7794789b4549cb323c5ba6aa49bfc0b08ee54e
emnetdegafe <emnetdegafe@gmail.com>
2020-06-22 21:56:33 +0200

    first migration

--- models/todoitem.js
+++ models/todoitem.js
@@ -2,7 +2,8 @@
 module.exports = (sequelize, DataTypes) => {
   const todoItem = sequelize.define('todoItem', {
     tasks: DataTypes.STRING,
-    deadline: DataTypes.STRING
+    deadline: DataTypes.STRING,
+    important: DataTypes.BOOLEAN
   }, {});
   todoItem.associate = function(models) {
     // associations can be defined here
