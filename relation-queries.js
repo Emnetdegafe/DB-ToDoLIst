988b9dc0acfade9c370af960c2b08234121b0e68
emnetdegafe <emnetdegafe@gmail.com>
2020-06-23 16:51:19 +0200

    user with ID with list that includes the task of the todoItem

--- relation-queries.js
+++ relation-queries.js
@@ -13,3 +13,29 @@ const userWithList = async (id) => {
   }
 }
 // userWithList(2)
+const importantItemWithList = async () => {
+  try {
+const ImportantItem = await todoItem.findAll({
+  where: {important:true},
+  include: [{model:todoList, attribute: ["name"]}]
+})
+console.log(ImportantItem.map((item) => item.get({plain:true}))
+  )
+  }catch(e) {
+    console.log('error', e)
+  }
+}
+// importantItemWithList()
+
+const userWithTasks = async (id) => {
+  try {
+const userById = await user.findByPk(id, {
+  include: [{model:todoList , include: [{model:todoItem, attribute: ["tasks"]}]}]
+}) 
+const plainUser = userById.get({plain:true})
+console.log(plainUser.todoLists[1].todoItems)
+  }catch(e) {
+    console.log('error', e)
+  }
+}
+userWithTasks(1)
